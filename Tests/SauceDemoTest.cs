388495816f98737878f94250c223bc88ae00dea0
ANAS KHAN <khananas280@gmail.com>
2025-07-16 02:18:31 +0530

    Adding information for understanding behavior

--- Tests/SauceDemoTest.cs
+++ Tests/SauceDemoTest.cs
@@ -18,9 +18,11 @@ namespace DecisionsAssesment.Tests
 
             await page.GotoAsync("https://www.saucedemo.com/");
 
+            // Call to Login page and details upload
             var loginPage = new LoginPage();
             await loginPage.Login(page, "standard_user", "secret_sauce");
 
+            // Inventory page product added to card any products.
             var inventoryPage = new InventoryPage();
             await inventoryPage.AddItemsToCart(page, 2);
             await inventoryPage.AssertCartItemCount(page, 2);
@@ -28,10 +30,12 @@ namespace DecisionsAssesment.Tests
             await inventoryPage.AssertCartItemCount(page, 1);
             await inventoryPage.OpenCart(page);
 
+            // checking out cart products are selected.
             var cartPage = new CartPage();
             await cartPage.AssertRemainingItem(page);
             await cartPage.Checkout(page);
 
+            // putting details and checking out.
             var checkoutPage = new CheckoutPage();
             await checkoutPage.FillCustomerInformation(page, "Anas", "Khan", "400001");
             await checkoutPage.AssertItemInOverview(page);
